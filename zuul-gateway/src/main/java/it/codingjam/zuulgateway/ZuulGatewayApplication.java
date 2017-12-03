c239cfd8c7c0a0c17dfe31bdfc254e77f345c8b6
andrea.como <andrea.como@gmail.com>
2017-12-03 01:56:36 +0100

    Enabled zuul project as zuul proxy

--- zuul-gateway/src/main/java/it/codingjam/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/it/codingjam/zuulgateway/ZuulGatewayApplication.java
@@ -3,7 +3,9 @@ package it.codingjam.zuulgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulGatewayApplication {
